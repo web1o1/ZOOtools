67ced15373c45f8d90d5b0803f8330f4cee7d905
Miljan <miljan@aleksic.es>
2013-01-22 00:02:47 +0100

    JEXEC security line was missing

--- plg_zootools/install.script.php
+++ plg_zootools/install.script.php
@@ -6,6 +6,9 @@
 * @license   	http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
 */
 
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
 jimport('joomla.filesystem.file');
 jimport('joomla.filesystem.folder');
 
